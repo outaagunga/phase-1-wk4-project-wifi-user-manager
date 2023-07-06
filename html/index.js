5bf42ce18a89c3a498c7d6b8c1d78716619b39be
outaagunga <jeconiaagunga@gmail.com>
2023-07-06 08:58:56 +0300

    updates

---
+++ html/index.js
@@ -0,0 +1,55 @@
+
+
+//Functin to direct people to Signup page 
+document.getElementById("packageDropdown").addEventListener("change", function() {
+  let selectedValue = this.value;
+
+  switch (selectedValue) {
+    case "basic":
+      window.location.href = "signup.html";
+      break;
+    case "standard":
+      window.location.href = "signup.html";
+      break;
+    case "premium":
+      window.location.href = "signup.html";
+      break;
+    default:
+      break;
+  }
+});
+
+
+
+// Fetch users from the local API
+fetch('http://localhost:3000/users')
+.then(response => response.json())
+.then(users => {
+// Get the users section and the users list
+const usersSection = document.getElementById('users');
+const usersList = document.getElementById('theusers');
+
+
+// Create a list item for each user
+users.forEach(user => {
+const listItem = document.createElement('li');
+listItem.textContent = user.name;
+usersList.appendChild(listItem);
+});
+
+
+// Append the users list to the users section
+usersSection.appendChild(usersList);
+})
+.catch(error => {
+console.error('Error fetching users:', error);
+});
+
+
+
+//directing to login page when click any part of the users
+let usersSection = document.getElementById('users');
+
+usersSection.addEventListener('click', function() {
+  window.location.href = 'login.html';
+});
